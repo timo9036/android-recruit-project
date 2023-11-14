0bbd17b016ab7160dce5708855e31cdd4ba6d1e3
timo9036 <126628160+timo9036@users.noreply.github.com>
2023-11-14 23:10:52 +0800

    Feat: Navigation. Added CourseFragment and xml, navgraph. Fixed gradle comflict issue with navigation

--- app/src/main/java/com/hahow/android_recruit_project/MainActivity.kt
+++ app/src/main/java/com/hahow/android_recruit_project/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hahow.android_recruit_project
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import `in`.hahow.android_recruit_project.R
 
 
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    override fun onStart() {
+        super.onStart()
+        val navController = findNavController(R.id.nav_host_fragment)
+        navController.navigate(R.id.courseFragment)
     }
 }
